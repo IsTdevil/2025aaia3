1f9408fe1eeeff26d7e3f783685f2129b7a24fc1
IsTdevil <euan1014@gmail.com>
2025-10-02 17:20:11 +0800

    week04

---
+++ week04/week04-4b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	while( cin >> a >>b ){
+		int ans = 0;
+		int now=1;
+		while(n != 1) {
+			if(n%2==1) n = 3*n+1;
+			now++;
+		}
+		if(now>ans) ans = now;
+		printf( "%d %d %d\n", a, b, ans);
+	}
+}
