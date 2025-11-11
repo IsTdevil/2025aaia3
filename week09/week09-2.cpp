cb0c93937052ab0d6275a1753421b9578f1b114f
IsTdevil <euan1014@gmail.com>
2025-11-11 21:48:48 +0800

    week09

---
+++ week09/week09-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int M = mat.size(), N = mat[0].size();
+        int ans = 0;
+        for(int i=0; i<M; i++){
+            for(int j=0; j<N; j++){
+                //if(¬O¹ï¨¤½uªº¸Ü) ans += mat[i][j];
+                if(i==j || i+j==N-1) ans += mat[i][j];
+            }
+        }
+        return ans;
+    }
+};
